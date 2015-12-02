5a0bb608c7a00ef93547f1dac210a0c7fc44ec8c
Tomas Radvansky <info@radvansky-solutions.com>
2015-12-02 09:10:33 +0100

    Initial commit

---
+++ SwiftQPXSample/Pods/Target Support Files/Pods-SwiftQPXSample/Pods-SwiftQPXSample-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SwiftQPXSampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SwiftQPXSampleVersionString[];
+
---
+++ SwiftQPXSample/SwiftQPXSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftQPXSample
+//
+//  Created by Tomas Radvansky on 02/12/2015.
+//  Copyright © 2015 radvansky.tomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
